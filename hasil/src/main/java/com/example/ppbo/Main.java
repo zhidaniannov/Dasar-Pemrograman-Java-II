22629eecc1a6dc26ce37f64c2ccd03b4d0f69ca9
muhammadradithia6081-dot <muhammad.radithia6081@student.unri.ac.id>
2025-09-09 11:14:19 +0700

    Add files via upload

--- hasil/src/main/java/com/example/ppbo/Main.java
+++ hasil/src/main/java/com/example/ppbo/Main.java
@@ -1,11 +1,7 @@
 package com.example.ppbo;
 
-/**
- * Author: Kelompok 5
- * Hasil dari praktikum Dasar Pemrograman Java II
- */
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.println("Hello world!");
     }
 }

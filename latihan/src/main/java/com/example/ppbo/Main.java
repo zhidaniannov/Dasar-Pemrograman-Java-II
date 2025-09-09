43289f7ba8835ef3cbc62714117ec4f66b420a40
Rifaul Khairunisa <rifaul.khairunisa0731@student.unri.ac.id>
2025-09-09 08:47:01 +0700

    Add files via upload

--- latihan/src/main/java/com/example/ppbo/Main.java
+++ latihan/src/main/java/com/example/ppbo/Main.java
@@ -1,7 +1,23 @@
 package com.example.ppbo;
-
-public class Main {
+import java.util.Scanner;
+public class Main 
+{
+    public static final double PI = 3.14;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("================Kalkulator Bola=================");
+        System.out.print("Masukkan jari-jari (cm): ");
+        double radius = scanner.nextDouble();
+        System.out.println("=================Hasil Perhitungan==============");
+
+        // Perhitungan volume dan luas permukaan bola
+        double volume = (4.0/3.0) * PI * radius * radius * radius;
+        double luasPermukaan = 4 * PI * radius * radius;
+
+        // Menampilkan hasil
+        System.out.println("Volume bola: " + volume + " cm³");
+        System.out.println("Luas permukaan bola: " + luasPermukaan + " cm²");
+        
+        scanner.close();
     }
 }

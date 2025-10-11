b4d13a4630a12b6a56cc620727c3bc915c0473de
muhammadradithia6081-dot <muhammad.radithia6081@student.unri.ac.id>
2025-10-11 10:17:00 +0700

    Add files via upload

--- praktikum/Produk.java
+++ praktikum/Produk.java
@@ -1,55 +1,25 @@
-public class Produk {
-    public String nama;
-    private double harga;
-    protected int stok;
-    private String namaSupplier = "rahif";
+public class Produk implements HargaAkhir {
 
-    static int jumlahProduk = 0;
+    protected String nama;
+    protected int harga;
 
-    
-    public Produk(String nama, double harga, int stok) {
+    public Produk(String nama, int harga) {
         this.nama = nama;
         this.harga = harga;
-        this.stok = stok;
-        jumlahProduk++;
     }
 
-    
-    public static void infoJumlahProduk() {
-        System.out.println("Total produk yang telah dibuat: " + jumlahProduk);
-    }
-
-    
-    private void namaSupplier() {
-        System.out.println("Nama Supplier: " + this.namaSupplier);
-    }
-
-    
-    public void namaSupplierFix() {
-        this.namaSupplier();
-    }
-
-   
-    public double getHarga() {
-        return this.harga;
-    }
-
-   
-    public void setHarga(double harga) {
-        if (harga > 0) {
-            this.harga = harga;
-        } else if (harga == 0) {
-            this.harga = harga;
-            System.out.println("Produk ini gratis");
-        } else {
-            System.out.println("Harga tidak boleh negatif");
-        }
-    }
-
-    
     public void tampilkanInfo() {
-        System.out.println("Nama Produk: " + this.nama);
-        System.out.println("Harga: " + this.harga);
-        System.out.println("Stok: " + this.stok);
+        System.out.println("Nama: " + nama);
+        System.out.println("Harga: " + harga);
+    }
+
+    public double hitungPajak(){
+        return harga * 0.05;
+    }
+
+    @Override
+    public double hitungHarga(){
+        return harga * 1.01;
     }
 }
+
